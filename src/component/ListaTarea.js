629cab30ce4571dabc719a34fc6f65d220ad4b19
Omar Garcia <za19012225@zapopan.tecmm.edu.mx>
2023-12-18 00:57:02 -0600

    Agregando funciones

--- src/component/ListaTarea.js
+++ src/component/ListaTarea.js
@@ -7,6 +7,10 @@ function ListaTarea()
 {
     const [tareas, setTareas] = useState([]);
 
+    const agregarTarea = tarea => {
+        console.log('Tarea agregada:', tarea);
+    };
+
     return (
         <>
             <TareaFormulario />
--- src/component/TareaFormulario.js
+++ src/component/TareaFormulario.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css';
 
 function TareaFormulario(props)
 {
+    const [input, setInput] = useState('');
+
+    const manejarCambio = e => {
+        setInput(e.target.value);
+        console.log(e.target.value);
+    };
+
+    const manejarEnvio = e => {
+        const tareaNueva = {
+            id: '12345',
+            texto: 'React Developer'
+        };
+    };
+
     return (
         <form className='tarea-formulario'>
             <input 
@@ -10,6 +24,8 @@ function TareaFormulario(props)
                 type='text'
                 placeholder='Escribe una tarea'
                 name='texto'
+                // Cuando ocurra un cambio en el formulario
+                onChange={manejarCambio}
             />
 
             <button className='tarea-boton'>Agregar Tarea</button>
