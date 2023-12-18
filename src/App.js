48c58aba5bc3ad5c77cbf18c76991770608965b8
Omar Garcia <za19012225@zapopan.tecmm.edu.mx>
2023-12-17 21:53:29 -0600

    Añadiendo CSS a TAREAS

--- src/App.js
+++ src/App.js
@@ -10,6 +10,10 @@ function App() {
           className='logo'
         />
       </div>
+
+      <div className='tareas-lista-principal'>
+        <h1>Mis tareas</h1>
+      </div>
     </div>
   );
 }
